40d1ee6bedf0099e53043c801a5fcf020f6f5e36
Kamal Haider <12868764+kamal-haider@users.noreply.github.com>
2025-12-03 19:05:22 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/KAMAL_PORTFOLIO',
   images: {
     unoptimized: true, // Required for static export
   },
